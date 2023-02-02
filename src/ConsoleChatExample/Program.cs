22515a07fd5034ec6c770152e7ccc8505a0f0ff8
Aux <justin@akitaux.com>
2023-02-02 01:39:43 -0600

    Add projects

---
+++ src/ConsoleChatExample/Program.cs
@@ -0,0 +1,77 @@
+﻿using AuxLabs.SimpleTwitch.Chat;
+
+var user = Environment.GetEnvironmentVariable("TWITCH_USER", EnvironmentVariableTarget.User);
+var token = Environment.GetEnvironmentVariable("TWITCH_TOKEN", EnvironmentVariableTarget.User);
+
+Console.WriteLine("> Initializing chat client...");
+if (user == null)
+{
+    Console.WriteLine("> Please enter your username: ");
+    user = Console.ReadLine();
+}
+if (token == null)
+{
+    Console.WriteLine("> Please enter your oauth token: ");
+    token = Console.ReadLine();
+}
+
+Console.WriteLine("> Please enter the channel name to join: ");
+var channelName = Console.ReadLine();
+
+Console.WriteLine("> Connecting...");
+var twitch = new TwitchChatApiClient()      // Create an instance of the chat client
+    .SetIdentity(user, token);              // Set the login information
+
+twitch.Connected += OnConnectedAsync;
+twitch.MessageReceived += OnMessageReceived;
+twitch.UserNoticeReceived += OnUserNoticeReceived;
+
+await twitch.RunAsync();                    // Run the connection loop, the app will lock here until the client is disposed.
+
+void OnConnectedAsync()                     // After connection is confirmed, join a channel
+{
+    Console.WriteLine("> Connected");
+    twitch.Send(new JoinChannelRequest(channelName));
+}
+
+void OnMessageReceived(MessageEventArgs args)
+{
+    var name = !string.IsNullOrWhiteSpace(args.Tags.DisplayName)     // Sometimes twitch doesn't send a display name
+        ? args.Tags.DisplayName 
+        : args.Tags.Login;
+
+    if (!string.IsNullOrWhiteSpace(args.Tags.ReplyParentMessageId))     // Check if the message is replying to another
+        Console.WriteLine($"#{args.ChannelName} {name} -> {args.Tags.ReplyParentUserName}: {args.Message}");
+    else
+        Console.WriteLine($"#{args.ChannelName} {name}: {args.Message}");
+}
+
+void OnUserNoticeReceived(UserNoticeEventArgs args)
+{
+    var name = !string.IsNullOrWhiteSpace(args.Tags.DisplayName)     // Sometimes twitch doesn't send a display name
+        ? args.Tags.DisplayName 
+        : args.Tags.Login;
+
+    switch (args.Tags)
+    {
+        case RaidTags tags:                 // Message indicates a raid
+            Console.WriteLine($"#{args.ChannelName} just got raided by {tags.RaiderDisplayName} with {tags.RaiderViewerCount} viewers!");
+            break;
+
+        case SubscriptionGiftTags tags:     // Message indicates a gifted sub
+            Console.WriteLine($"#{args.ChannelName} {name} just gifted {tags.RecipientDisplayName} a sub!");
+            break;
+
+        case SubscriptionTags tags:         // Message indicates a paid sub
+            if (tags.NoticeType == UserNoticeType.Subscription)
+                Console.WriteLine($"#{args.ChannelName} {name} just subscribed for {tags.TotalMonths} months: {args.Message}");
+            else
+                Console.WriteLine($"#{args.ChannelName} {name} just resubscribed for {tags.TotalMonths} months: {args.Message}");
+            break;
+
+        default:                            // Other non-special notices
+            if (args.Tags.NoticeType == UserNoticeType.Announcement)
+                Console.WriteLine($"#{args.ChannelName} {name} ANNOUNCES: {args.Message}");
+            break;
+    }
+}
---
+++ src/ConsoleRestExample/Program.cs
@@ -0,0 +1,62 @@
+﻿using AuxLabs.SimpleTwitch.Rest;
+using System.Net.Http.Headers;
+
+var pass = Environment.GetEnvironmentVariable("TWITCH_TOKEN", EnvironmentVariableTarget.User);
+var clientId = Environment.GetEnvironmentVariable("TWITCH_CLIENTID", EnvironmentVariableTarget.User);
+
+Console.WriteLine("> Initializing rest client...");
+if (pass == null)
+{
+    Console.WriteLine("> Please enter your oauth token: ");
+    pass = Console.ReadLine();
+}
+if (clientId == null)
+{
+    Console.WriteLine("> Please enter your client id: ");
+    clientId = Console.ReadLine();
+}
+
+while (true)                                            // Just a loop so you can request multiple users
+{
+    Console.WriteLine("> Please enter the username you want to get info for: ");
+    var userName = Console.ReadLine();
+
+    var twitch = new TwitchRestApiClient                // Create the client with authentication headers
+    {
+        Authorization = new AuthenticationHeaderValue("Bearer", pass),
+        ClientId = clientId
+    };
+
+    var userResponse = await twitch.GetUsersAsync(x =>      // Get the user by name
+    {
+        x.UserNames.Add(userName);
+    });
+
+    if (userResponse.Data.FirstOrDefault() is User user)    // Check if the user exists
+    {
+        var channelResponse = await twitch.GetChannelsAsync(x =>      // Get the user's channel
+        {
+            x.ChannelIds.Add(user.Id);
+        });
+
+                                                            // Output their user info
+        Console.WriteLine($"{user.DisplayName ?? user.Login} ({user.Id})");
+        if (!string.IsNullOrWhiteSpace(user.Description))
+            Console.WriteLine($"{user.Description}");
+        Console.WriteLine($"They joined at {user.CreatedAt}");
+        if (user.BroadcasterType != BroadcasterType.None)
+            Console.WriteLine($"They currently have {user.BroadcasterType} status");
+        if (user.Type != UserType.None)
+            Console.WriteLine($"They are a {user.Type}");
+
+        var channel = channelResponse.Data.First();             // Output their channel info
+        if (channel.GameId != null)
+            Console.WriteLine($"They were last playing {channel.GameName} ({channel.GameId})");
+        if (channel.Tags.Any())
+            Console.WriteLine($"With the tags: {string.Join(", ", channel.Tags)}");
+
+        Console.ReadKey(true);
+    }
+    else
+        Console.WriteLine($"{userName} does not exist!");
+}
