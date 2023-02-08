61d68b64116b94eaad49018140177ce27cada7ae
Aux <justin@akitaux.com>
2023-02-07 21:03:24 -0600

    Update to latest

--- src/ConsoleChatExample/Program.cs
+++ src/ConsoleChatExample/Program.cs
@@ -1,4 +1,5 @@
-﻿using AuxLabs.SimpleTwitch.Chat;
+﻿using AuxLabs.SimpleTwitch;
+using AuxLabs.SimpleTwitch.Chat;
 
 var user = Environment.GetEnvironmentVariable("TWITCH_USER", EnvironmentVariableTarget.User);
 var token = Environment.GetEnvironmentVariable("TWITCH_TOKEN", EnvironmentVariableTarget.User);
@@ -19,59 +20,35 @@ Console.WriteLine("> Please enter the channel name to join: ");
 var channelName = Console.ReadLine();
 
 Console.WriteLine("> Connecting...");
-var twitch = new TwitchChatApiClient()      // Create an instance of the chat client
-    .SetIdentity(user, token);              // Set the login information
+
+// Create an instance of the chat client and set identity
+var twitch = new TwitchChatApiClient();
+twitch.SetIdentity(user, token);
 
 twitch.Connected += OnConnectedAsync;
 twitch.MessageReceived += OnMessageReceived;
-twitch.UserNoticeReceived += OnUserNoticeReceived;
 
-await twitch.RunAsync();                    // Run the connection loop, the app will lock here until the client is disposed.
+// Run the connection loop, the app will lock here until the client is disposed.
+await twitch.RunAsync();
 
-void OnConnectedAsync()                     // After connection is confirmed, join a channel
+// After connection is confirmed, join a channel
+void OnConnectedAsync()                     
 {
     Console.WriteLine("> Connected");
     twitch.Send(new JoinChannelRequest(channelName));
 }
 
+// Handle when a message is received
 void OnMessageReceived(MessageEventArgs args)
 {
-    var name = !string.IsNullOrWhiteSpace(args.Tags.DisplayName)     // Sometimes twitch doesn't send a display name
-        ? args.Tags.DisplayName 
-        : args.Tags.Login;
+    // Sometimes twitch doesn't send a display name, so we use name otherwise.
+    var name = !string.IsNullOrWhiteSpace(args.Tags.DisplayName)
+        ? args.Tags.DisplayName
+        : args.Tags.Name;
 
-    if (!string.IsNullOrWhiteSpace(args.Tags.ReplyParentMessageId))     // Check if the message is replying to another
+    // Check if the message is replying to another
+    if (!string.IsNullOrWhiteSpace(args.Tags.ReplyParentMessageId))     
         Console.WriteLine($"#{args.ChannelName} {name} -> {args.Tags.ReplyParentUserName}: {args.Message}");
     else
         Console.WriteLine($"#{args.ChannelName} {name}: {args.Message}");
-}
-
-void OnUserNoticeReceived(UserNoticeEventArgs args)
-{
-    var name = !string.IsNullOrWhiteSpace(args.Tags.DisplayName)     // Sometimes twitch doesn't send a display name
-        ? args.Tags.DisplayName 
-        : args.Tags.Login;
-
-    switch (args.Tags)
-    {
-        case RaidTags tags:                 // Message indicates a raid
-            Console.WriteLine($"#{args.ChannelName} just got raided by {tags.RaiderDisplayName} with {tags.RaiderViewerCount} viewers!");
-            break;
-
-        case SubscriptionGiftTags tags:     // Message indicates a gifted sub
-            Console.WriteLine($"#{args.ChannelName} {name} just gifted {tags.RecipientDisplayName} a sub!");
-            break;
-
-        case SubscriptionTags tags:         // Message indicates a paid sub
-            if (tags.NoticeType == UserNoticeType.Subscription)
-                Console.WriteLine($"#{args.ChannelName} {name} just subscribed for {tags.TotalMonths} months: {args.Message}");
-            else
-                Console.WriteLine($"#{args.ChannelName} {name} just resubscribed for {tags.TotalMonths} months: {args.Message}");
-            break;
-
-        default:                            // Other non-special notices
-            if (args.Tags.NoticeType == UserNoticeType.Announcement)
-                Console.WriteLine($"#{args.ChannelName} {name} ANNOUNCES: {args.Message}");
-            break;
-    }
 }
